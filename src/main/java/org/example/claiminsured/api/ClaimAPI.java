d5bc036e758e59e4c5daa1295cc2dbfcb290334e
Jagmeetkang <Jagmeetkang01@gmail.com>
2025-01-31 14:58:15 -0500

    WebClient

--- src/main/java/org/example/claiminsured/api/ClaimAPI.java
+++ src/main/java/org/example/claiminsured/api/ClaimAPI.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.Duration;
 import java.util.List;
 
 @RestController
@@ -17,12 +19,24 @@ public class ClaimAPI {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient webClient;
+
 
     @GetMapping("/get")
     public String getDuration(){
 
-        String str = restTemplate.getForObject("http://localhost:8082/warrentyapi/byId/1", String.class);
-        return str;
+        //String str = restTemplate.getForObject("http://localhost:8082/warrentyapi/byId/1", String.class);
+        String warrenty = webClient.get().uri("http://localhost:8082/warrentyapi/byId/1")
+                .header("token", "7687g78yg88g9u")
+                .header("username", "adminuser")
+                .header("password" ,"pragra123")
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofMillis(10000))
+                .block();
+
+        return warrenty;
     }
 
     @GetMapping("/getById")
